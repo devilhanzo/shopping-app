4d4109e1c02897fc19a04d29b16006209cdcc3c6
devilhanzo <skulchai.ch@gmail.com>
2017-07-20 16:30:58 +0700

    Update recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Cookie', 'Cokie wiht coffee', 'http://delivery-menu.exteen.com/images/theme/donutmuffins_front.jpg'),
+  ];
   constructor() { }
 
   ngOnInit() {
+    console.log(this.recipes[0].name);
   }
 
 }
